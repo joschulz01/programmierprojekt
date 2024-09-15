eb76e8316bae9b08985e306147e518abdb7440e4
joschul <schulzejonah@web.de>
2024-09-16 01:12:19 +0200

    feat: add menu buttons

--- programmierprojekt/src/app/app.component.ts
+++ programmierprojekt/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
+import { MenuComponent } from "./menu/menu.component";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
