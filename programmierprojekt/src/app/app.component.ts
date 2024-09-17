a7f330a069564e4bf1637b9448dd9cf90e2e6864
Nadine <nadine.arning@hs-osnabrueck.de>
2024-09-17 22:08:37 +0200

    Solver Eingabe hinzugefüft

--- programmierprojekt/src/app/app.component.ts
+++ programmierprojekt/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { LanguageSelectorComponent } from './language-selector/language-selector.component';
 import { GlpkComponent } from './glpk/glpk.component';
+import { SolverComponent } from './solver/solver.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, LanguageSelectorComponent, GlpkComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, LanguageSelectorComponent, GlpkComponent, SolverComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
