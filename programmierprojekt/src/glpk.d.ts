2e7583088b8852c53749a08110fad088954278de
Nadine <nadine.arning@hs-osnabrueck.de>
2024-09-19 09:15:42 +0200

    Für Lint angepasst

--- programmierprojekt/src/glpk.d.ts
+++ programmierprojekt/src/glpk.d.ts
@@ -14,7 +14,7 @@ declare module 'glpk.js' {
     export function glp_set_obj_dir(prob: unknown, dir: number): void;
     export function glp_set_obj_name(prob: unknown, name: string): void;
     export function glp_add_cols(prob: unknown, n: number): number;
-    export function glp_set_col_name(prob: unknown, col: number, name: string): void;
+    export function glp_set_col_name(prob: unknown, col: number, name): void;
     export function glp_set_col_bnds(prob: unknown, col: number, type: number, lb: number, ub: number): void;
     export function glp_add_rows(prob: unknown, n: number): number;
     export function glp_set_row_name(prob: unknown, row: number, name: string): void;
@@ -22,7 +22,7 @@ declare module 'glpk.js' {
     export function glp_set_mat_row(prob: unknown, row: number, ne: number, col: number[], val: number[]): void;
     export function glp_simplex(prob: unknown, params: unknown): void;
     export function glp_get_num_cols(prob: unknown): number;
-    export function glp_get_col_name(prob: unknown, col: number): string;
+    export function glp_get_col_name(prob: unknown, col: number);
     export function glp_get_col_prim(prob: unknown, col: number): number;
     export function glp_delete_prob(prob: unknown): void;
   }
