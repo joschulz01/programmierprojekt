3c76ed1a6732214624db667b0f3b7946aa1d3703
joschul <schulzejonah@web.de>
2024-09-24 00:48:42 +0200

    feat: add HiGHS solver component

--- programmierprojekt/src/app/app.component.ts
+++ programmierprojekt/src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FormsModule} from '@angular/forms';
 import { HeaderComponent } from "./header/header.component";
+import { HighsSolverComponent} from "./highs-solver/highs-solver.component";
 import { MenuComponent } from "./menu/menu.component";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MenuComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterOutlet,
+    HeaderComponent,
+    HighsSolverComponent,
+    MenuComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
