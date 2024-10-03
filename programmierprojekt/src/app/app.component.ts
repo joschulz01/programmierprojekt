46499fe1af8180de03e4a91868538a9af56c45fd
Fabienne Priemer <fabienne.priemer@hs-osnabrueck.de>
2024-10-03 13:20:41 +0200

    component und Import Button erstellt

--- programmierprojekt/src/app/app.component.ts
+++ programmierprojekt/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { AboutUsComponent } from './about-us/about-us.component';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DatenschutzComponent } from './datenschutz/datenschutz.component';
 import { FeedbackComponent } from './feedback/feedback.component';
 import { FooterComponent } from './footer/footer.component';
 import {FormsModule} from '@angular/forms';
 import { HeaderComponent } from "./header/header.component";
 import { HighsSolverComponent} from "./highs-solver/highs-solver.component";
 import { HomeComponent } from './home/home.component';
+import { ImpressumComponent } from './impressum/impressum.component';
 import { MenuComponent } from "./menu/menu.component";
 import { RouterOutlet } from '@angular/router';
 import { ModelComponent } from './model/model.component';
@@ -17,6 +19,7 @@ import { ModelComponent } from './model/model.component';
   imports: [
     AboutUsComponent,
     CommonModule,
+    DatenschutzComponent,
     FeedbackComponent,
     FooterComponent,
     FormsModule,
@@ -24,6 +27,7 @@ import { ModelComponent } from './model/model.component';
     HeaderComponent,
     HighsSolverComponent,
     HomeComponent,
+    ImpressumComponent,
     MenuComponent,
     ModelComponent
   ],
