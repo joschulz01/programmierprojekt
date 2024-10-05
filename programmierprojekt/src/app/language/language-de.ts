5cc33482cd03965630a79b3c23d48ba4c61795cd
Christianvoss2002 <christian.voss@kampmann.de>
2024-10-05 13:27:29 +0200

    Implement feedback translation feature

--- programmierprojekt/src/app/language/language-de.ts
+++ programmierprojekt/src/app/language/language-de.ts
@@ -1,5 +1,9 @@
 export const DE_TRANSLATIONS = {
   title: 'Operations Research Tool',
   subtitle: 'Programmierprojekt 2024',
-  switchLanguage: 'Sprache wechseln'
+  switchLanguage: 'Sprache wechseln',
+  feedbackTitle: 'Feedback zur Anwendung',
+  feedbackDescription: 'Bitte teilen Sie uns Ihre Gedanken, Vorschläge oder Verbesserungsvorschläge mit. Ihr Feedback ist uns sehr wichtig!',
+  feedbackPlaceholder: 'Hier haben Sie die Möglichkeit, Ihr Feedback zu hinterlegen...',
+  submitFeedback: 'Absenden',
 };
