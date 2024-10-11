7e0ddd53377a59946ef13b2581b8368adf32e3a0
Fabienne Priemer <fabienne.priemer@hs-osnabrueck.de>
2024-10-11 21:45:59 +0200

    Formatierung und Unit Tests

--- programmierprojekt/src/app/app.component.spec.ts
+++ programmierprojekt/src/app/app.component.spec.ts
@@ -1,11 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, RouterTestingModule],
+      imports: [AppComponent, Router],
     }).compileComponents();
   });
 
